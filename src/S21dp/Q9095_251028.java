50637367ea461f35e152b312832c98a099a94059
NAHO <ohmap@naver.com>
2025-10-28 19:29:10 +0900

    Solve - BOJ 9095 - 1,2,3 더하기

---
+++ src/S21dp/Q9095_251028.java
@@ -0,0 +1,35 @@
+package S21dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q9095_251028 {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[12];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for(int i = 4; i <= 11; i++){
+            dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+        }
+
+        for(int i = 0; i < t; i++){
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n]).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+}
